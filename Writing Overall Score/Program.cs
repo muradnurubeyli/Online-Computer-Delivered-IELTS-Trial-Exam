8a83e05198fccad042c0b4f2a6be3703085cab2a
Murad Nurubeyli <48731292+muradnurubeyli@users.noreply.github.com>
2020-05-21 20:58:06 +0400

    finding writing overall score

---
+++ Writing Overall Score/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Writing_Overall_Score
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double task1 = Convert.ToDouble(Console.ReadLine());
+            double task2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = ((task1 * 1) + (task2 * 2)) / 3;
+
+            Console.WriteLine(((int)(result * 4 + 1) / 2) / 2.0);
+        }
+    }
+}
