88da8f6e6b9e44532f62ab894c4b703f63733b33
Murad Nurubeyli <48731292+muradnurubeyli@users.noreply.github.com>
2020-05-21 17:53:08 +0400

    IELTS overall score calculator
    Formula for calculating overall IELTS scores based on average of 4 sections.

---
+++ Score Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Score_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double listening = Convert.ToDouble(Console.ReadLine());
+            double reading = Convert.ToDouble(Console.ReadLine());
+            double writing = Convert.ToDouble(Console.ReadLine());
+            double speaking = Convert.ToDouble(Console.ReadLine());
+
+            double average = (listening + reading + writing + speaking) / 4;
+            Console.WriteLine(((int)(average * 4 + 1) / 2) / 2.0);
+
+        }
+    }
+}
